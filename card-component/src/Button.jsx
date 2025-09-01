4711cd540f60b7ab753784fe7d1d3c5df283fe2c
nycwy <stuxy.1129@gmail.com>
2025-09-01 17:20:04 +0530

    Button component using inline css

---
+++ card-component/src/Button.jsx
@@ -0,0 +1,12 @@
+export default function Button() {
+
+    const styles = {
+        backgroundColor: "green",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer"
+    }
+    return (<button style={styles}>Click me!</button>)
+}
