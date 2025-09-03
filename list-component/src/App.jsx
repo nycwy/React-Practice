303961d07653946f29f2b82f43a37c12a8ea321f
nycwy <stuxy.1129@gmail.com>
2025-09-03 13:03:13 +0530

    Reusable List using (Props)

--- list-component/src/App.jsx
+++ list-component/src/App.jsx
@@ -1,7 +1,27 @@
 import List from "./List"
 
 export default function App() {
+
+  const fruits = [
+    { id: 1, name: "Apple", calories: 95 },
+    { id: 2, name: "Banana", calories: 105 },
+    { id: 3, name: "Kiwi", calories: 44 },
+    { id: 4, name: "Mango", calories: 99 },
+    { id: 5, name: "Pineapple", calories: 37 }
+  ];
+
+  const vegetables = [
+    { id: 6, name: "Potatoes", calories: 110 },
+    { id: 7, name: "Celery", calories: 15 },
+    { id: 8, name: "Carrots", calories: 25 },
+    { id: 9, name: "Corn", calories: 63 },
+    { id: 10, name: "Broccoli", calories: 50 }
+  ];
+
   return (
-    <List />
+    <>
+      <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
+    </>
   )
 }
--- list-component/src/List.jsx
+++ list-component/src/List.jsx
@@ -1,20 +1,11 @@
-export default function List() {
-    const fruits = [
-        { id: 1, name: "Apple", calories: 95 },
-        { id: 2, name: "Banana", calories: 105 },
-        { id: 3, name: "Kiwi", calories: 44 },
-        { id: 4, name: "Mango", calories: 99 },
-        { id: 5, name: "Pineapple", calories: 37 }
-    ];
-
-    // fruits.sort((a, b)=> a.name.localeCompare(b.name)) //Alphabetical
-    // fruits.sort((a, b) => b.name.localeCompare(a.name)) //Reverse Alphabetical
-    // fruits.sort((a, b) => a.calories - b.calories); //Numeric
-    // fruits.sort((a, b) => b.calories - a.calories); //Reverse Numeric
-
-    // const lowCalFruits = fruits.filter(lowCalFruit => lowCalFruit.calories < 50);
-    // const highCalFruits = fruits.filter(highCalFruit => highCalFruit.calories > 50);
+export default function List(props) {
     
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b></li>)
-    return (<ul>{listItems}</ul>);
+    const itemList = props.items;
+    const listItems = itemList.map(item => <li key={item.id}>{item.name}: &nbsp; <b>{item.calories}</b></li>)
+    return (
+        <>
+            <h2>{props.category}</h2>
+            <ul>{listItems}</ul>
+        </>
+    );
 }
