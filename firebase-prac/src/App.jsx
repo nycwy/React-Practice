1a626b25c74c92dcbe5b199aa75014f6ae974db3
Nycwy <stuxy.1129@gmail.com>
2026-01-31 16:54:05 +0545

    Firebase (Read Operation)

--- firebase-prac/src/App.jsx
+++ firebase-prac/src/App.jsx
@@ -1,9 +1,10 @@
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { db } from "./firebase";
 
 function App() {
     const [message, setMessage] = useState('');
+    const [posts, setPosts] = useState([]);
 
     const handleAddPost = async () => {
         if (!message) return;
@@ -19,10 +20,38 @@ function App() {
         }
     }
 
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const postsRef = collection(db, "posts");
+                const postsDoc = await getDocs(postsRef);
+                if (!postsDoc.empty) {
+                    const posts = postsDoc.docs.map((doc) => ({
+                        id: doc.id,
+                        ...doc.data()
+                    }));
+                    setPosts(posts);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getPosts();
+    }, []);
+
     return (
         <>
             <input value={message} type="text" name="post" id="post" placeholder="Post" onChange={(e) => setMessage(e.target.value)} />
             <button type="submit" onClick={handleAddPost}>Add Post</button>
+            <h1>All Posts</h1>
+            <div>
+                {
+                    posts.map((post) => {
+                        return <div key={post.id}>{post.message}</div>
+                    })
+                }
+            </div>
         </>
     )
 }
