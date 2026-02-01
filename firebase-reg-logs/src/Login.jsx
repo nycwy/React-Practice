492d08ee92d4f9a1e0af26c41d35bc587d1ffe75
Nycwy <stuxy.1129@gmail.com>
2026-02-02 00:35:03 +0545

    Login added!

--- firebase-reg-logs/src/Login.jsx
+++ firebase-reg-logs/src/Login.jsx
@@ -1,7 +1,41 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "./firebase";
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    }
+
     return (
-        <div>Login</div>
-    )
-}
+        <div>
+            <input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+            />
+
+            <input
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+            />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+};
 
 export default Login;
