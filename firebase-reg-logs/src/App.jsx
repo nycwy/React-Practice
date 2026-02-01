5749269451fb305e4c3f7e55fc2f97db3f2459e0
Nycwy <stuxy.1129@gmail.com>
2026-02-02 00:26:18 +0545

    Logout added

--- firebase-reg-logs/src/App.jsx
+++ firebase-reg-logs/src/App.jsx
@@ -1,13 +1,24 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from './firebase';
 
 function App() {
     const { user } = useContext(AuthContext);
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    }
     
     return (
         <>
             <div>{user?.email}</div>
             <div>{user?.username}</div>
+            <button onClick={handleLogout}>Logout</button>
         </>
     )
 }
