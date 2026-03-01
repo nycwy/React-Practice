7bcf65ba5e18c061bb6e0f13eece659888c7eb22
Nycwy <stuxy.1129@gmail.com>
2026-03-01 23:35:12 +0545

    .

--- advice-app/src/App.jsx
+++ advice-app/src/App.jsx
@@ -4,25 +4,25 @@ import { fetchAdvice } from './redux/adviceSlice'
 import Advice from './components/Advice'
 
 function App() {
-  const dispatch = useDispatch();
-  const { adviceText, isLoading, error } = useSelector((state) => state.advice);
+    const dispatch = useDispatch();
+    const { adviceText, isLoading, error } = useSelector((state) => state.advice);
 
-  useEffect(() => {
-    dispatch(fetchAdvice());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchAdvice());
+    }, [dispatch]);
 
-  const handleNewAdvice = () => {
-    dispatch(fetchAdvice());
-  };
+    const handleNewAdvice = () => {
+        dispatch(fetchAdvice());
+    };
 
-  return (
-    <Advice
-      advice={adviceText}
-      isLoading={isLoading}
-      onFetchNew={handleNewAdvice}
-      error={error}
-    />
-  )
+    return (
+        <Advice
+            advice={adviceText}
+            isLoading={isLoading}
+            onFetchNew={handleNewAdvice}
+            error={error}
+        />
+    )
 }
 
 export default App
